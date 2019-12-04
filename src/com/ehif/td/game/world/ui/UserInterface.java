57a73dbe6a1a86a850d56b16555b4290d3082846
BuildTools <unconfigured@null.spigotmc.org>
2019-12-04 09:07:33 +0100

    Format

--- src/com/ehif/td/game/world/ui/UserInterface.java
+++ src/com/ehif/td/game/world/ui/UserInterface.java
@@ -7,21 +7,25 @@ import java.util.ArrayList;
 public class UserInterface {
     private int offset;
     private ArrayList<Button> buttons = new ArrayList<Button>();
-    public UserInterface(int offset){
+
+    public UserInterface(int offset) {
         this.offset = offset;
     }
-    public UserInterface(){
+
+    public UserInterface() {
         this.offset = 0;
     }
-    public void add(Button b){
+
+    public void add(Button b) {
         buttons.add(b);
     }
-    public void display(Sketch s) throws NullPointerException{
-        try{
-            for(Button b:buttons){
+
+    public void display(Sketch s) throws NullPointerException {
+        try {
+            for (Button b : buttons) {
                 b.display(s);
             }
-        }catch(NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("NullPointerException in UserInterface @ line 22");
         }
     }
